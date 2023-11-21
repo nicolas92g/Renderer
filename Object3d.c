5a714c8fb14a5b1f9f3ca0181281a82e718e4d67
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2023-11-21 22:34:32 +0100

    set vars as const in Object3dDraw

--- Object3d.c
+++ Object3d.c
@@ -12,11 +12,11 @@ void Object3dCreate(Object3d* self,Mesh* mesh, vec4 position, vec4 scale, vec4 r
 void Object3dDraw(Object3d* self, Shader* shader)
 {
 
-    mat4 rot = rotationMatrix(self->rotationAxis, self->rotationAngle);
-    mat4 trans = translationMatrix(self->position.x, self->position.y, self->position.z);
-    mat4 scale = scaleMatrix(self->scale.x, self->scale.y, self->scale.z);
-    mat4 tmp = Mat4Multiply(&trans, &rot);
-    mat4 model = Mat4Multiply(&tmp, &scale);
+    const mat4 rot = rotationMatrix(self->rotationAxis, self->rotationAngle);
+    const mat4 trans = translationMatrix(self->position.x, self->position.y, self->position.z);
+    const mat4 scale = scaleMatrix(self->scale.x, self->scale.y, self->scale.z);
+    const mat4 tmp = Mat4Multiply(&trans, &rot);
+    const mat4 model = Mat4Multiply(&tmp, &scale);
 
     ShaderSetMat4(shader, "model", &model);
 
